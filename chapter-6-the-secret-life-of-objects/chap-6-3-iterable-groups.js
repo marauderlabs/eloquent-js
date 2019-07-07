e163d548b17f4184d1098466494c162781d73a9e
Anand <anand@rtbrick.com>
2019-07-07 16:08:05 +0530

    Ch6 - ex 3

---
+++ chapter-6-the-secret-life-of-objects/chap-6-3-iterable-groups.js
@@ -0,0 +1,44 @@
+/*
+    Iterable groups
+
+    Make the Group class from the previous exercise iterable.
+    Refer to the section about the iterator interface earlier
+    in the chapter if you aren’t clear on the exact form of the interface anymore.
+
+    If you used an array to represent the group’s members,
+    don’t just return the iterator created by calling the Symbol.iterator method 
+    on the array. That would work, but it defeats the purpose of this exercise.
+
+    It is okay if your iterator behaves strangely when the group is modified 
+    during iteration.
+*/
+
+const Group = require("./chap-6-2-groups")
+
+
+class GroupIterator{
+    constructor(group){
+        this.group = group;
+        this.cursor = 0;
+    }
+    next(){
+        if (this.cursor == this.group.length) {
+            return {done: true}
+        }
+
+        return {value: this.group.get(this.cursor++), done: false};
+    }
+}
+
+Group.prototype[Symbol.iterator] = function() {
+    return new GroupIterator(this);
+}
+
+if (require.main === module) {
+    for (let value of Group.from(["a", "b", "c"])) {
+        console.log(value);
+    }
+    // → a
+    // → b
+    // → c
+}
